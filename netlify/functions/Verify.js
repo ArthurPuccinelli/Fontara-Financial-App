67d7ad158815e6079659bed08e529d4032394c78
ArthurPuccinelli <51099983+ArthurPuccinelli@users.noreply.github.com>
2025-04-23 08:21:35 -0300

    Update Verify.js
    Signed-off-by: ArthurPuccinelli <51099983+ArthurPuccinelli@users.noreply.github.com>

--- netlify/functions/Verify.js
+++ netlify/functions/Verify.js
@@ -2,33 +2,29 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
   try {
-    // Verificar se a entrada está no formato correto
     console.log("Recebido:", event);
 
-    // Validar o tipo de entrada
-    if (!event.data || !event.data.clienteId) {
+    const body = JSON.parse(event.body);
+    if (!body.data || !body.data.clienteId) {
       throw new Error("clienteId é obrigatório");
     }
 
-    // Chamar a API para verificar os dados do cliente (simulando uma API externa)
     const response = await fetch('https://fontarafinancial.netlify.app/.netlify/functions/verificaCPFeCNPJ', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        clienteId: event.data.clienteId,
+        clienteId: body.data.clienteId,
       }),
     });
 
     const data = await response.json();
 
-    // Verificar a resposta da API
     if (!response.ok) {
       throw new Error(`Erro ao verificar dados: ${data.error || 'Desconhecido'}`);
     }
 
-    // Retornar a resposta com os dados
     return {
       statusCode: 200,
       body: JSON.stringify({

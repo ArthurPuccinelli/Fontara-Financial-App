fae2939cc2c862e0f65aff4111a95779d9bb0830
ArthurPuccinelli <51099983+ArthurPuccinelli@users.noreply.github.com>
2025-04-25 14:11:05 -0300

    Update GetTypeNames.js
    Signed-off-by: ArthurPuccinelli <51099983+ArthurPuccinelli@users.noreply.github.com>

--- netlify/functions/GetTypeNames.js
+++ netlify/functions/GetTypeNames.js
@@ -2,17 +2,15 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
   try {
-    // Cliente de exemplo
     const clienteId = '22222222222';
 
-    // Chamada à função HTTP correta
     const response = await fetch(`https://fontarafinancial.netlify.app/.netlify/functions/verificaCPFeCNPJHandler?cliente_id=${clienteId}`);
     const data = await response.json();
     console.log("Resposta da API:", data);
-    // Gerar typeNames dinamicamente a partir das chaves retornadas pela API
+
     const typeNames = Object.keys(data).map((key) => ({
-      typeName: key.charAt(0).toUpperCase() + key.slice(1),
-      label: key.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())
+      typeName: key, // mantém o camelCase original
+      label: key.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase()) // bonito no rótulo
     }));
 
     return {

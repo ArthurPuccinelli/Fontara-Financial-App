32eb042f4cebc6a2216757967851413184b5ad97
ArthurPuccinelli <51099983+ArthurPuccinelli@users.noreply.github.com>
2025-04-29 19:47:59 -0300

    Delete terraform directory
    Signed-off-by: ArthurPuccinelli <51099983+ArthurPuccinelli@users.noreply.github.com>

--- terraform/common/modules/generate/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "random_bytes" {
-  description = "The generated random bytes"
-  value       = local.random_bytes[var.random_bytes_type]
-}
--- terraform/common/modules/template/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "output_file_path" {
-  description = "The absolute path to the output file"
-  value       = local.output_file_path
-}

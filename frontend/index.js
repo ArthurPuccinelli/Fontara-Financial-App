c45e7a14accf420467252c2cdad80813656ad60d
ArthurPuccinelli <51099983+ArthurPuccinelli@users.noreply.github.com>
2025-05-10 19:34:10 +0000

    interface

--- frontend/index.js
+++ frontend/index.js
@@ -245,24 +245,28 @@ gsap.to("#dashboard", {
 
 const faqAccordion = document.querySelectorAll('.faq-accordion')
 
-faqAccordion.forEach(btn => {
+faqAccordion.forEach(function (btn) {
     btn.addEventListener('click', function () {
-        const content = this.nextElementSibling
-        const icon = this.querySelector('.bi-plus')
+        this.classList.toggle('active')
 
-        const isOpen = content.style.maxHeight && content.style.maxHeight !== '0px'
+        // Toggle 'rotate' class to rotate the arrow
+        let content = this.nextElementSibling
+        let icon = this.querySelector(".bi-plus")
 
-        if (isOpen) {
+        // content.classList.toggle('!tw-hidden')
+        if (content.style.maxHeight === '240px') {
             content.style.maxHeight = '0px'
             content.style.padding = '0px 18px'
-            icon.style.transform = 'rotate(0deg)'
+            icon.style.transform = "rotate(0deg)"
+            
         } else {
-            content.style.maxHeight = content.scrollHeight + 'px'
+            content.style.maxHeight = '240px'
             content.style.padding = '20px 18px'
-            icon.style.transform = 'rotate(45deg)'
+            icon.style.transform = "rotate(45deg)"
         }
     })
 })
+
 
 
 // ------------- reveal section animations ---------------
